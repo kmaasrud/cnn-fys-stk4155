3e72a4c7c6a14dcaebcf978c7571ec34bf743b7e
philipkarim <54434877+philipkarim@users.noreply.github.com>
2020-12-10 01:12:47 +0100

    The accuracy is too good, training set wont load right

--- src/logreg_facemask.py
+++ src/logreg_facemask.py
@@ -1,8 +1,8 @@
-
-"""
 #Import modules
 import sklearn.linear_model as lm
 from sklearn.utils import shuffle
+from sklearn.model_selection import  train_test_split
+from sklearn.preprocessing import scale, StandardScaler
 
 from skimage import io
 from skimage.transform import resize
@@ -20,60 +20,17 @@ import random
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
-random.seed(2020)
 
-# load the image
-#image = Image.open('00034.jpg')
-# summarize some details about the image
-#print(image.format)
-#print(image.mode)
-#print(image.size)
-# show the image
-#image.show()
+#Load the data
+X_train_mask=np.load('X_train_mask.npy')
+X_train_nomask=np.load('X_train_nomask.npy')
+X_test_mask=np.load('X_test_mask.npy')
+X_test_nomask=np.load('X_test_nomask.npy')
 
-#Defining lists
-train_mask_list = []
-train_nomask_list = []
-test_mask_list = []
-test_nomask_list = []
-
-#Extract the images from the folders and makes them into 100x100sizes for faster
-#handling of the images, can also try with thumbnails(Keeping the ratio)
-
-for filename in glob.glob('facemasks/data/train/with_mask/*'): #assuming gif
-    im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
-    im2 /= 255.
-    train_mask_list.append(im2)
-
-for filename in glob.glob('facemasks/data/train/without_mask/*'): #assuming gif
-    im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
-    im2 /= 255.
-    train_nomask_list.append(im2)
-
-for filename in glob.glob('facemasks/data/test/with_mask/*'): #assuming gif
-    im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
-    im2 /= 255.
-    test_mask_list.append(im2)
-
-for filename in glob.glob('facemasks/data/test/without_mask/*'): #assuming gif
-    im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
-    im2 /= 255.
-    test_nomask_list.append(im2)
-
-
-
-#Saving the lists in arrays
-#Might have to concatenate train and test, and split it up into a bigger testset
-
-X_train_mask = np.asarray(train_mask_list)
-X_train_nomask = np.asarray(train_nomask_list)
-X_test_mask = np.asarray(test_mask_list)
-X_test_nomask = np.asarray(test_nomask_list)
-#X_test_nomask=test_nomask_list
+print(len(X_train_mask))
+print(len(X_train_nomask))
+print(len(X_test_mask))
+print(len(X_test_nomask))
 
 #Making y values: 1= mask, 0=no masks
 y_train_mask=np.ones(len(X_train_mask))
@@ -87,47 +44,26 @@ X_test = np.concatenate((X_test_mask, X_test_nomask), axis=0)
 y_train= np.concatenate((y_train_mask, y_train_nomask), axis=None)
 y_test= np.concatenate((y_test_mask, y_test_nomask), axis=None)
 
+X_train, y_train = shuffle(X_train, y_train, random_state=0)
+X_test, y_test = shuffle(X_test, y_test, random_state=0)
 
-#Shuffling the images
-X_train_scaled, y_train = shuffle(X_train_scaled, y_train, random_state=0)
-X_test_scaled, y_test = shuffle(X_test_scaled, y_test, random_state=0)
+"""Increase size of test set"""
+X=np.concatenate((X_train,X_test), axis=0)
+y=np.concatenate((y_train,y_test), axis=None)
 
-# Reshape the data, and convert to grayscale
-#X_test_nomask= rgb2gray(X_test_nomask)
-print(X_test_nomask.shape)
-image_shape = X_test_nomask.shape[1:]
-IMG_HEIGHT = image_shape[0]
-IMG_WIDTH = image_shape[1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
-print(IMG_HEIGHT)
+y_train=np.ravel(y_train)
+y_test=np.ravel(y_test)
 
-#Converting to greyscale?
-x_train = rgb2gray(x_train)
-x_test = rgb2gray(x_test)
+# Scaling the data using the scikit learn modules
+scaler = StandardScaler();  scaler.fit(X_train)
+X_train= scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
-colours=3
-
-#Ravels and reshaping
-X_train.ravel()
-X_test.ravel()
-
-#or is the colours allready in the array, so we dont need to multiply with 3??
-X_train_reshape=X_train.reshape(X_train.shape[0],image.shape[1]*image.shape[2])
-X_test_reshape= X_test.reshape(X_test.shape[0],X_test.shape[1]*X_test.shape[2])
-
-np.save('X_train.npy', X_train_reshape)
-np.save('X_test.npy', X_test_reshape)
-np.save('t_train.npy', y_train)
-np.save('y_test.npy', y_test)
-"""
-
-#Load the data
-
-X_train=np.load('X_train.npy')
-X_test=np.load('X_test.npy')
-y_train=np.load('y_train.npy')
-y_train=np.load('y_test.npy')
-
+#Raveling
+#X_train=np.ravel(X_train)
+#X_test=np.ravel(X_test)
 
 #Just a function to get some overview of the dataset
 def info_about_the_data(y_train=y_train, y_test=y_test):
@@ -179,4 +115,4 @@ def log_reg_scikit_learn(X_train=X_train, X_test=
 
 #Calling the functions
 #info_about_the_data()
-#log_reg_scikit_learn()
+log_reg_scikit_learn()
--- src/logreg_maskdata.py
+++ src/logreg_maskdata.py
@@ -20,6 +20,8 @@ import matplotlib.pyplot as plt
 
 random.seed(2020)
 
+size=128
+
 #Defining lists
 train_mask_list = []
 train_nomask_list = []
@@ -27,44 +29,35 @@ test_mask_list = []
 test_nomask_list = []
 
 #Extract the images from the folders and makes them into 100x100sizes for faster
-#handling of the images, can also try with thumbnails(Keeping the ratio)
+#handling of the images.
+
 
 for filename in glob.glob('facemasks/data/train/with_mask/*'): #assuming gif
     im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
+    im2 = resize(im2, (size, size), preserve_range=True, mode='constant')
     im2 /= 255.
     train_mask_list.append(im2)
 
 for filename in glob.glob('facemasks/data/train/without_mask/*'): #assuming gif
     im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
+    im2 = resize(im2, (size, size), preserve_range=True, mode='constant')
     im2 /= 255.
     train_nomask_list.append(im2)
 
 for filename in glob.glob('facemasks/data/test/with_mask/*'): #assuming gif
     im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
+    im2 = resize(im2, (size, size), preserve_range=True, mode='constant')
     im2 /= 255.
+    #im2 = np.asarray(im2)
     test_mask_list.append(im2)
 
 for filename in glob.glob('facemasks/data/test/without_mask/*'): #assuming gif
     im2=io.imread(filename)
-    im2 = resize(im2, (128, 128), preserve_range=True, mode='constant')
+    im2 = resize(im2, (size, size), preserve_range=True, mode='constant')
     im2 /= 255.
+    #im2 = np.asarray(im2)
     test_nomask_list.append(im2)
 
-
-#How to resize imread???
-"""
-fig, axes = plt.subplots(4, 5, figsize=(20, 20))
-ax = axes.ravel()
-images = data.lfw_subset()
-for i in range(20):
-    ax[i].imshow(images[90+i], cmap=plt.cm.gray)
-    ax[i].axis('off')
-fig.tight_layout()
-plt.show()
-"""
 
 #Saving the lists in arrays
 """Might have to concatenate train and test, and split it up into a bigger testset"""
@@ -74,48 +67,59 @@ X_train_nomask = np.asarray(train_nomask_list)
 X_test_mask = np.asarray(test_mask_list)
 X_test_nomask = np.asarray(test_nomask_list)
 #X_test_nomask=test_nomask_list
-
-#Making y values: 1= mask, 0=no masks
-y_train_mask=np.ones(len(X_train_mask))
-y_train_nomask=np.zeros(len(X_train_nomask))
-y_test_mask=np.ones(len(X_test_mask))
-y_test_nomask=np.zeros(len(X_test_nomask))
-
+"""
 #Setting up the training and test sets
 X_train = np.concatenate((X_train_mask, X_train_nomask), axis=0)
 X_test = np.concatenate((X_test_mask, X_test_nomask), axis=0)
 y_train= np.concatenate((y_train_mask, y_train_nomask), axis=None)
 y_test= np.concatenate((y_test_mask, y_test_nomask), axis=None)
 
+"""
 
+#print(y_test)
+#print(X_test)
 #Shuffling the images
-X_train_scaled, y_train = shuffle(X_train_scaled, y_train, random_state=0)
-X_test_scaled, y_test = shuffle(X_test_scaled, y_test, random_state=0)
-
+#X_train, y_train = shuffle(X_train, y_train, random_state=0)
+#X_test, y_test = shuffle(X_test, y_test, random_state=0)
+#print(y_test)
+#print(X_test)
 # Reshape the data, and convert to grayscale
-#X_test_nomask= rgb2gray(X_test_nomask)
-print(X_test_nomask.shape)
-image_shape = X_test_nomask.shape[1:]
-IMG_HEIGHT = image_shape[0]
-IMG_WIDTH = image_shape[1]
+#X_test= rgb2gray(X_test)
+#X_test_mask= rgb2gray(X_test_mask)
 
-print(IMG_HEIGHT)
+#X_train_shape= X_train.shape[1:]
+#X_test_shape= X_test.shape[1:]
+#X_test_shape= X_test_mask.shape[1:]
 
 #Converting to greyscale?
-x_train = rgb2gray(x_train)
-x_test = rgb2gray(x_test)
+#X_train_grey = rgb2gray(X_train)
+# = rgb2gray(X_test)
 
-colours=3
+
+X_train_mask= rgb2gray(X_train_mask)
+X_train_nomask = rgb2gray(X_train_nomask)
+X_test_mask = rgb2gray(X_test_mask)
+X_test_nomask= rgb2gray(X_test_nomask)
+
+
 
 #Ravels and reshaping
-X_train.ravel()
-X_test.ravel()
+#X_train.ravel()
+#X_test=np.ravel(X_test)
+#X_test_mask=np.ravel(X_test_mask)
+
+""" Ravel etterpå, concatenate, shuffle"""
 
 #or is the colours allready in the array, so we dont need to multiply with 3??
-X_train_reshape=X_train.reshape(X_train.shape[0],image.shape[1]*image.shape[2])
-X_test_reshape= X_test.reshape(X_test.shape[0],X_test.shape[1]*X_test.shape[2])
+#X_train_reshape=X_train.reshape(X_train.shape[0],X_train_shape[1]*X_train_shape[2])
+X_train_mask= X_train_mask.reshape(X_train_mask.shape[0],size*size)
+X_train_nomask= X_train_nomask.reshape(X_train_nomask.shape[0],size*size)
+X_test_mask= X_test_mask.reshape(X_test_mask.shape[0],size*size)
+X_test_nomask= X_test_nomask.reshape(X_test_nomask.shape[0],size*size)
 
-np.save('X_train.npy', X_train_reshape)
-np.save('X_test.npy', X_test_reshape)
-np.save('t_train.npy', y_train)
-np.save('y_test.npy', y_test)
+#X_test_reshape= X_test.reshape(X_test.shape[0],size*size)
+
+np.save('X_train_mask.npy', X_test_mask)
+np.save('X_train_nomask.npy', X_train_nomask)
+np.save('X_test_mask.npy', X_test_mask)
+np.save('X_test_nomask.npy', X_test_nomask)
