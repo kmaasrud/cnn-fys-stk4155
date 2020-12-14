5c4daa390ddea3c268bbc63f24b8209e0bbdd655
kmaasrud <km@aasrud.com>
2020-12-14 20:21:08 +0100

    99% accuracy on test set, 50% on real pictures

--- src/facemasks/predict.py
+++ src/facemasks/predict.py
@@ -11,10 +11,13 @@ sys.path.insert(0, parentdir)
 
 from cnn import CNN
 
-data = Data.load_from_imgs("real_pics")
 
 cnn = CNN.load("models/facemask_cnn2")
 
+data = Data.load_from_npy("data/preprocessed")
 predictions = data.postprocess(cnn.model.predict(data.X_test))
+print(f"Test set accuracy: {data.accuracy(predictions)}")
 
-data.accuracy(predictions, print_report=True)
+data = Data.load_from_imgs("real_pics")
+predictions = data.postprocess(cnn.model.predict(data.X_test))
+print(f"Real pictures accuracy: {data.accuracy(predictions)}")
